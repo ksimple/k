2b0fed1c1c0bd6f0077b6a641364f0d9c7f4797f
Bin Du <bindu@ksimple.org>
2015-12-06 09:30:40 +0800

    .

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+﻿module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        srcFiles: [
+            'src/*.ts',
+        ],
+        ts: {
+            debug: {
+                src: ['src/*.ts'],
+                out: 'build/kLayouter.js',
+                options: {
+                    target: 'es5',
+                    declaration: true,
+                    removeComments: false,
+                },
+            },
+        },
+        uglify: {
+            ship: {
+                files: {
+                    'build/kLayouter.min.js': ['<%= ts.debug.out %>'],
+                },
+            },
+        },
+        jasmine: {
+            debug: {
+                src: '<%= ts.debug.out %>',
+                options: {
+                    specs: 'test/*.spec.js',
+                },
+            },
+            ship: {
+                src: 'build/kLayouter.min.js',
+                options: {
+                    specs: 'test/*.spec.js',
+                },
+            }
+        },
+        watch: {
+            ts: {
+                files: ['<%= ts.debug.src %>'],
+                tasks: ['uglify:ship', 'jasmine:ship']
+            },
+            uglify: {
+                files: ['<%= ts.debug.out %>'],
+                tasks: ['ts:debug', 'jasmine:ship']
+            },
+            test_debug: {
+                files: ['<%= jasmine.debug.options.specs %>'],
+                tasks: ['jasmine:debug'],
+            },
+            test_ship: {
+                files: ['<%= jasmine.ship.options.specs %>'],
+                tasks: ['jasmine:ship'],
+            },
+        },
+    });
+
+    grunt.registerTask('debug', ['ts:debug']);
+    grunt.registerTask('ship', ['debug', 'uglify:ship']);
+    grunt.registerTask('build', ['debug', 'ship']);
+    grunt.registerTask('test', ['jasmine:debug', 'jasmine:ship']);
+    grunt.registerTask('all', ['build', 'test']);
+
+    // Default task(s).
+    grunt.registerTask('default', 'No default task', function() {
+        grunt.log.write('The old default task has been moved to "build" to prevent accidental triggering');
+    });
+};
---
+++ README.md
@@ -0,0 +1,15 @@
+Dependency injection - Invoker
+===
+Build
+---
+```
+npm install
+grunt debug
+grunt test
+```
+APIs
+---
+
+Examples
+---
+
